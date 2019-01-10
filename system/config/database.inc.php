28dedb2b9c5216a90586c239813e390ce8bc1dfd
Charles Deng <chenxideng1026@gmail.com>
2019-01-10 17:57:03 +0800

    initialize push.

---
+++ system/config/database.inc.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+'default' => array (
+'hostname' => '192.168.0.253','database' => 'ceshi','username' => 'root','password' => '901026DCXdcx','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysqli','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+);
---
+++ system/config/version.inc.php
@@ -0,0 +1,1 @@
+<?php  return array("version"=>"V1.0.0","release"=>"20190101"); ?>
