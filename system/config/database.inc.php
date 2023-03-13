548704447faac8bd52e9f1078f37385552b51a01
chenxideng <chenxideng1026@gmail.com>
2023-03-13 14:22:37 +0800

    Update database.inc.php

--- system/config/database.inc.php
+++ system/config/database.inc.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 'default' => array (
-'hostname' => 'localhost','database' => 'onedollar','username' => 'root','password' => 'password','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysqli','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+'hostname' => 'localhost','database' => 'onedollar','username' => 'root','password' => '901026DCXdcx','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysqli','debug' => false,'pconnect' => 0,'autoconnect' => 0),
 );
